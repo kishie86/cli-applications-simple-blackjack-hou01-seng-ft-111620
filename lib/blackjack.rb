6e54675179ef115abd46a882dca9dc8127c2f644
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-14 21:32:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -89,12 +89,13 @@ def runner
   count = 0 
   welcome
   initial_round
-  prompt_user
+  hit?(current_card_total)
   
   until count >= 21
   hit?(current_card_total)
   # code runner here
   count += 1
 end
+end_game
 end
     
